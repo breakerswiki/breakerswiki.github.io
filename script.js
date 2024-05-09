e1dbd80f45d8982d642d71621d64e3f6caf0d3f3
breakerswiki <zoukken88@gmail.com>
2024-05-09 18:14:33 +0200

    update

--- script.js
+++ script.js
@@ -58,4 +58,13 @@ window.addEventListener("hashchange", removeHash);
 
 
 
+// When the user scrolls down 300px from the top of the document, slide down the navbar
+window.onscroll = function() {scrollFunction()};
 
+function scrollFunction() {
+  if (document.body.scrollTop > 300 || document.documentElement.scrollTop > 300) {
+    document.getElementById("navbar").style.top = "0";
+  } else {
+    document.getElementById("navbar").style.top = "-50px";
+  }
+}
